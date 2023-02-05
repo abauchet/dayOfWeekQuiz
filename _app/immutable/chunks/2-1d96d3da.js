b59b12085e6824c62315980f9ea91cac62fb13b5
Arnaud Bauchet <abauchet@users.noreply.github.com>
2023-02-05 11:14:48 +0100

    Updates

---
+++ _app/immutable/chunks/2-1d96d3da.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-e9f07b9c.js";export{t as component};
--- _app/immutable/chunks/2-8c29ae88.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-25642b79.js";export{t as component};
